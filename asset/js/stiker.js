06d8f18ab7bace5fe62fdfe439fe2a1076982cfe
Alejandro Perez <alejandro8001@gmail.com>
2024-01-13 13:19:14 -0300

    listo el contador de stikers

---
+++ asset/js/stiker.js
@@ -0,0 +1,20 @@
+const contardor = document.querySelector(".button-1");
+let valor1 = document.querySelector(".form-control-1");
+let valor2 = document.querySelector(".form-control-2");
+let valor3 = document.querySelector(".form-control-3");
+
+contardor.addEventListener("click", contarStiker);
+
+function contarStiker() {
+  let texto = document.querySelector(".texto-h3");
+  let numero1 = valor1.value;
+  let numero2 = valor2.value;
+  let numero3 = valor3.value;
+
+  const numero4 = +numero1 + +numero2 + +numero3;
+  if (numero4 <= 10) {
+    texto.innerHTML = "llevas : " + numero4 + " de stiker";
+  } else {
+    texto.innerHTML = "Llevas demaciados stiker";
+  }  
+}
