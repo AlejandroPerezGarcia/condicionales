a277f8f53bb3d2aea87393d70f4f61563cd4a1de
Alejandro Perez <alejandro8001@gmail.com>
2024-01-16 20:33:37 -0300

    modificacion js passord

--- asset/js/password.js
+++ asset/js/password.js
@@ -7,59 +7,40 @@ let card1 = document.querySelector(".card-1");
 num.addEventListener("click", validar);
 
 
-function validar (){
-    contra1();
-    contra2();
-    contra3();
-}
 
-
-function contra1() {
+function validar() {
   let digitoA = digito1.value;
+  let digitoB = digito2.value;
+  let digitoC = digito3.value;
   let num1 = document.querySelector(".num-1");
+  let num2 = document.querySelector(".num-2");
+  let num3 = document.querySelector(".num-3");
   const afirma = "Numero Ingresado es el cororecto";
   const nega = "Numero Ingresado es incorrecto";
   const dig1 = 9;
+  const dig2 = 1;
+  const dig3 = 1;
 
   if (dig1 == digitoA) {
     num1.innerHTML = afirma;
   } else {
     num1.innerHTML = nega;
-  }
+  };
+
+   if (dig2 == digitoB) {
+     num2.innerHTML = afirma;
+   } else {
+     num2.innerHTML = nega;
+   };
+
+    if (dig3 == digitoC) {
+      num3.innerHTML = afirma;
+    } else {
+      num3.innerHTML = nega;
+    };
 
   console.log(digitoA );
+  console.log(digitoB);
+  console.log(digitoC);
 };
 
-function contra2() {
-  
-  let digitoB = digito2.value;  
-  let num2 = document.querySelector(".num-2");
-  const afirma = "Numero Ingresado es el cororecto";
-  const nega = "Numero Ingresado es incorrecto";
-  const dig2 = 1;
-
-  if (dig2 == digitoB) {
-    num2.innerHTML = afirma;
-  } else {
-    num2.innerHTML = nega;
-  }
-
-  console.log( digitoB );
-};
-
-function contra3() {
- 
-  let digitoC = digito3.value;
-  let num3 = document.querySelector(".num-3");
-  const afirma = "Numero Ingresado es el cororecto";
-  const nega = "Numero Ingresado es incorrecto";
-  const dig3 = 1;
-
-  if (dig3 == digitoC) {
-    num3.innerHTML = afirma;
-  } else {
-    num3.innerHTML = nega;
-  }
-
-  console.log( digitoC);
-};
