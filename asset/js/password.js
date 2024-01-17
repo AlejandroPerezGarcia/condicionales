1f0f4ed1631621e14e37accdb9c3a17bfa42c09f
Alejandro Perez <alejandro8001@gmail.com>
2024-01-16 21:22:35 -0300

    modificacion Password hecho como lo solicitaban el anterior mal hecho

--- asset/js/password.js
+++ asset/js/password.js
@@ -12,35 +12,28 @@ function validar() {
   let digitoA = digito1.value;
   let digitoB = digito2.value;
   let digitoC = digito3.value;
-  let num1 = document.querySelector(".num-1");
-  let num2 = document.querySelector(".num-2");
-  let num3 = document.querySelector(".num-3");
-  const afirma = "Numero Ingresado es el cororecto";
-  const nega = "Numero Ingresado es incorrecto";
-  const dig1 = 9;
-  const dig2 = 1;
-  const dig3 = 1;
+  let mensaje = document.querySelector(".mensaje"); 
+  const codigo1 = "Password 1 correcto";
+  const codigo2 = "Password 2 correcto";
+  const codigo3 = "Password incorrecto";  
+  const password1 = 911;
+  const password2 = 714;
 
-  if (dig1 == digitoA) {
-    num1.innerHTML = afirma;
-  } else {
-    num1.innerHTML = nega;
-  };
+  let a = digitoA + digitoB + digitoC;
 
-   if (dig2 == digitoB) {
-     num2.innerHTML = afirma;
-   } else {
-     num2.innerHTML = nega;
-   };
+ 
 
-    if (dig3 == digitoC) {
-      num3.innerHTML = afirma;
-    } else {
-      num3.innerHTML = nega;
-    };
+  if (password1 == a) {
+    console.log(a);
+    mensaje.innerHTML = codigo1;
+  } else if (password2 == a) {
+    mensaje.innerHTML = codigo2;
+  }else {
+     mensaje.innerHTML= codigo3;
+     
+  }
 
-  console.log(digitoA );
-  console.log(digitoB);
-  console.log(digitoC);
+
+
 };
 
